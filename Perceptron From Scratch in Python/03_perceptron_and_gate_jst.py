c10f76e1f96641de44a05878b319046f0777662e
ridwaanhall <erbyl14@gmail.com>
2023-11-22 15:45:25 +0700

    jst pert 3

--- Perceptron From Scratch in Python/03_perceptron_and_gate_jst.py
+++ Perceptron From Scratch in Python/03_perceptron_and_gate_jst.py
@@ -1,56 +1,61 @@
-# Example: perceptron learning - 1
-# Suppose we want to train a perceptron to compute AND
-'''
-training set:
-x1 = 1, x2 = 1 -> y = 1
-x1 = 1, x2 = -1 -> y = -1
-x1 = -1, x2 = 1 -> y = -1
-x1 = -1, x2 = -1 -> y = -1
-
-randomly, let:
-w0 = -0.9
-w1 = 0.6
-w2 = 0.2
-
-using these weights:
-x1 = 1, x2 = 1: -0.9*1 + 0.6*1 + 0.2*1 = -0.1  -1 WRONG
-x1 = 1, x2 = -1: -0.9*1 + 0.6*1 + 0.2*-1 = -0.5  -1 OK
-x1 = -1, x2 = 1: -0.9*1 + 0.6*-1 + 0.2*1 = -1.3  -1 WRONG
-x1 = -1, x2 = -1: -0.9*1 + 0.6*-1 + 0.2*-1 = -1.7  -1 OK
-
-'''
 # %%
 import numpy as np
 
 # %%
+# AND Gate
 features = np.array([
-    [1, 1],
-    [1, -1],
-    [-1, 1],
-    [-1, -1]
+    [-1, -1, 1],
+    [-1, 1, 1],
+    [1, -1, 1],
+    [1, 1, 1]
 ])
 
 # %%
-w = [-0.9, 0.6, 0.2]
-
-# %%
+# Target with labels -1 and 1
 labels = np.array([
-    1,
     -1,
     -1,
-    -1
+    -1,
+    1
 ])
 
 # %%
-# make code from 'using there weights:'
-for i in range(features.shape[0]):
-    instance = features[i]
-    x0 = instance[0]
-    x1 = instance[1]
-    sum_unit = w[0] * x0 + w[1] * x1 + w[2]
-    if sum_unit > 0:
-        prediction = 1
-    else:
-        prediction = -1
-    print(f'x1 = {x0}, x2 = {x1}: {sum_unit} -> {prediction}')
-    
+w = [0.6, 0.2, -0.9]  # Including bias term
+learning_rate = 0.5  # Set the learning rate to 0.5
+epochs = 10
+
+# %%
+for j in range(epochs):
+    print('Epoch: ', j)
+    global_delta = 0
+    for i in range(features.shape[0]):
+
+        actual = labels[i]
+        instance = features[i]
+
+        x0 = instance[0]
+        x1 = instance[1]
+        bias = instance[2]
+
+        sum_unit = (w[0] * x0) + (w[1] * x1) + (w[2] * bias)
+
+        if sum_unit > 0:
+            prediction = 1
+        else:
+            prediction = -1
+
+        delta = actual - prediction
+        global_delta += abs(delta)
+
+        print(f'Prediction: {prediction} (Target: {actual}), Error: {delta}')
+        w[0] += delta * learning_rate * x0
+        w[1] += delta * learning_rate * x1
+        w[2] += delta * learning_rate * bias
+
+    print('-' * 20)
+
+    if global_delta == 0:
+        break
+
+print("Final Weights:", w)
+# %%
